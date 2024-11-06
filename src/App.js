8254fdd7b292fbb65c509dfe8347e651dc34c181
MohamedAmineMACHFER <amine.machfer1@gmail.com>
2024-11-06 01:07:57 +0100

    zxz

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <div className={'flex flex-col items-center justify-center gap-2'}>
+              <img src="/essquis-logo.svg" width={370} height={150} alt="Essquis Logo"/>
+              <div className={'flex gap-6  mt-[-1rem]'}>
+                  <span className={'text-xs cursor-pointer hover:text-gray-500 hover:text-sm'}>Home</span>
+                  <span className={'text-xs cursor-pointer hover:text-gray-500 hover:text-sm'}>Interior </span>
+                  <span className={'text-xs cursor-pointer hover:text-gray-500 hover:text-sm'}>Infos</span>
+                  <span className={'text-xs cursor-pointer hover:text-gray-500 hover:text-sm'}>Contact</span>
+              </div>
+          </div>
+
+      </div>
   );
 }
 
